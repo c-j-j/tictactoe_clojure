4886e6627f140cc4e217e170227aff57363cfb16
= <chrisjordan1987@gmail.com>
2015-04-14 10:19:10 +0100

    humanplayer checks move within range

--- src/tictactoe/humanplayer.clj
+++ src/tictactoe/humanplayer.clj
@@ -6,6 +6,7 @@
 (declare is-integer?)
 (declare get-input)
 (declare move-valid?)
+(declare move-in-range?)
 (declare get-valid-move)
 
 (defn get-move [board]
@@ -23,13 +24,17 @@
 (defn move-valid? [move board]
   (if (and
         (is-integer? move)
-        (move-available? board (parse-int move))) true false))
+        (move-available? board (parse-int move))
+        (move-in-range? board (parse-int move))) true false))
 
 (defn is-integer? [s]
   (boolean (re-matches #"\d+" s)))
 
 (defn move-available? [board move]
   (nil? (get board move)))
+
+(defn move-in-range? [board move]
+  (> (count board) move))
 
 (defn parse-int [s]
   (Integer/parseInt (re-find #"\A-?\d+" s)) )
--- test/tictactoe/humanplayer_test.clj
+++ test/tictactoe/humanplayer_test.clj
@@ -5,7 +5,7 @@
 
 (facts "about humanplayer"
   (fact "gets user input from console"
-    (with-in-str "1" (get-move [0])) => 1)
+    (with-in-str "0" (get-move [nil])) => 0)
 
   (fact "does not return non-integer input"
     (with-in-str "a\n0" (get-move [nil])) => 0
@@ -13,4 +13,9 @@
 
   (fact "move not valid when move exists"
     (with-in-str "0\n1" (get-move [:X nil])) => 1)
+
+  (fact "move not valid when it exceeds board size"
+    (with-in-str "-1\n0" (get-move [nil])) => 0
+    (with-in-str "1\n0" (get-move [nil])) => 0
+    )
   )
